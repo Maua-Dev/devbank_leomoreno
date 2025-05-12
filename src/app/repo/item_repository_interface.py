c6b428adc345824e9f8af22ffbce322cbc3e5507
Leonardo Moreno <leonardoa.moreno07@Gmail.com>
2025-05-11 23:55:33 -0300

    Fifth commit, bugs fixed

--- src/app/repo/item_repository_interface.py
+++ src/app/repo/item_repository_interface.py
@@ -7,15 +7,15 @@ from ..entities.item import Item
 
 
 class IItemRepository(ABC):
-    
-    
+
+
     @abstractmethod
     def get_all_items(self) -> List[Item]:
         '''
-        Returns all the itens in the database 
+        Returns all the itens in the database
         '''
         pass
-    
+
     @abstractmethod
     def get_item(self, item_id: int) -> Optional[Item]:
         '''
@@ -23,21 +23,21 @@ class IItemRepository(ABC):
         If the item does not exist, returns None
         '''
         pass
-    
+
     @abstractmethod
     def create_item(self, item: Item, item_id: int) -> Item:
         '''
         Creates a new item in the database
         '''
         pass
-    
+
     @abstractmethod
     def delete_item(self, item_id: int) -> Item:
         '''
         Deletes the item with the given id.
         If the item does not exist, returns None
         '''
-        
+
     @abstractmethod
     def update_item(self, item_id:int, name:str=None, price:float=None, item_type:ItemTypeEnum=None, admin_permission:bool=None) -> Item:
         '''
@@ -45,5 +45,4 @@ class IItemRepository(ABC):
         If the item does not exist, returns None
         '''
         pass
-    
-    
+
